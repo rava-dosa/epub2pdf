14bd51c1388e2b9fd6c318c537d81184f5541665
Apoorva Kumar <apoorvakumar169@gmail.com>
2020-07-26 15:49:37 +0530

    working for a lot of versions

--- epub2pdf.py
+++ epub2pdf.py
@@ -9,18 +9,62 @@ import os
 from inspect import currentframe, getframeinfo
 global_root_dir=""
 filename=""
+opf_name=""
 def read_css():
+    global global_root_dir
+    global opf_name
     font_config = FontConfiguration()
     ret=[]
-    try:
-        l=listdir(global_root_dir+"css")
-        for x in l:
-            ret.append(global_root_dir+"css/"+x)
-    except:
+    f=open(global_root_dir+opf_name,"r")
+    soup = BeautifulSoup(f.read(), 'lxml')
+    a=soup.find("manifest")
+    css_file=a.find("item",{"media-type":"text/css"}).get("href")
+    if(css_file is None):
         print("Css not found")
+    else:
+        ret.append(global_root_dir+css_file)
     return ret
 
+def get_ncx():
+    global global_root_dir
+    global opf_name
+    f=open(global_root_dir+"META-INF/"+"container.xml", "r")
+    data=f.read()
+    soup = BeautifulSoup(data, 'html.parser')
+    l=soup.find_all("rootfile")
+    if(len(l)==0):
+        print("no rootfile found")
+        quit()
+    else:
+        l1=l[0]
+        pathx=l1.get("full-path")
+        if("/" in pathx):
+            splitx=pathx.split("/")
+            global_root_dir= global_root_dir+splitx[0]+"/"
+            opf_name=splitx[1]
+            f=open(global_root_dir+splitx[1],"r")
+            soup = BeautifulSoup(f.read(), 'lxml')
+            a=soup.find("manifest")
+            ncx_file=a.find(id="ncx").get("href")
+            if(ncx_file is None):
+                print("NCX file not found")
+                quit()
+            f=open(global_root_dir+ncx_file,"r")
+            return f.read()
+        else:
+            opf_name=pathx
+            f=open(global_root_dir+pathx,"r")
+            soup = BeautifulSoup(f.read(), 'lxml')
+            a=soup.find("manifest")
+            ncx_file=a.find(id="ncx").get("href")
+            if(ncx_file is None):
+                print("NCX file not found")
+                quit()
+            f=open(global_root_dir+ncx_file,"r")
+            return f.read()
+        
 def writepdf(file_data):
+    global global_root_dir
     font_config=FontConfiguration()
     css=read_css()
     html=HTML(string=file_data)
@@ -28,15 +72,17 @@ def writepdf(file_data):
     html.write_pdf(filename+'.pdf', stylesheets=css,font_config=font_config)
 
 def generatepdf():
+    global global_root_dir
+    data=get_ncx()
     f=open(global_root_dir+"temp.xhtml","w")
-    try:
-        with open (global_root_dir+"toc.ncx", "r") as myfile:
-            data=myfile.read()
-    except:
-        frameinfo = getframeinfo(currentframe())
-        print("NCX file not found, quitting")
-        print(frameinfo.filename, frameinfo.lineno)
-        quit()
+    # try:
+    #     with open (global_root_dir+"toc.ncx", "r") as myfile:
+    #         data=myfile.read()
+    # except:
+    #     frameinfo = getframeinfo(currentframe())
+    #     print("NCX file not found, quitting")
+    #     print(frameinfo.filename, frameinfo.lineno)
+    #     quit()
     soup = BeautifulSoup(data, 'html.parser')
     l=soup.find_all("content")
     prev=""
@@ -53,10 +99,14 @@ def generatepdf():
     f.close()
     f=open(global_root_dir+"temp.xhtml","r")
     writepdf(f.read())
+
+
 def extract_zip_to_temp(path):
     with zipfile.ZipFile(path, 'r') as zip_ref:
         ret=zip_ref.extractall("/tmp/epub_temp/")
         print(ret)
+
+
 if __name__ == "__main__": 
     unzip_file_path=sys.argv[1]
     filename=unzip_file_path.split("/")[-1]
@@ -72,7 +122,7 @@ if __name__ == "__main__":
         print("")
     os.mkdir('/tmp/epub_temp')
     extract_zip_to_temp("/tmp/epub_temp.zip")
-    global_root_dir="/tmp/epub_temp/OEBPS/"
+    global_root_dir="/tmp/epub_temp/"
     # print(unzip_file_path)
     filename=filename[:-4]
     generatepdf()
